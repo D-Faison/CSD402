67c53af3fa2d097b34d15fee19bca6329ff09af5
DFaison <dfaison2017@gmail.com>
2025-03-19 15:53:29 -0400

    Create EnergyHeatWater.java

---
+++ M1/EnergyHeatWater.java
@@ -0,0 +1,39 @@
+/* DeJanae Faison
+ DATE
+ Assignment M1 
+ Description: Calculate the energy needed to heat water from an intial temp
+ to a final temp */
+
+import javax.swing.JOptionPane;
+
+public class EnergyHeatWater {
+    public static void main(String[] args) {
+        
+        //Variable Input and convert holding
+        
+        double waterMass;
+        String waterMassInput;
+
+        double intialTemp;
+        String intialTempInput;
+
+        double finalTemp;
+        String finalTempInput;
+
+        //final Double heatCapacity = 4186;
+
+        //Request Input through displays
+        waterMassInput = JOptionPane.showInputDialog(null, "Enter Mass of water in Kilograms: ",
+         "Water Mass Input",JOptionPane.QUESTION_MESSAGE);
+        //convert input to double
+        waterMass = Double.parseDouble(waterMassInput);
+
+        intialTempInput = JOptionPane.showInputDialog(null, "Enter Initial Temperature in C: ",
+         "Initial Temp Input",JOptionPane.QUESTION_MESSAGE);
+        intialTemp = Double.parseDouble(intialTempInput);
+
+        finalTempInput = JOptionPane.showInputDialog(null, "Enter FINAL Temperature in C: ",
+         "Final Temp Input",JOptionPane.QUESTION_MESSAGE);
+        finalTemp = Double.parseDouble(finalTempInput);
+    }
+}
